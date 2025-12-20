ac1b3bd471a225c0e7bb73c99905dd209f70da49
Chen Haoquan <chenhq79@mail2.sysu.edu.cn>
2025-12-21 00:08:11 +0800

    [FIX] small fix text render

--- src/renderer/TextRenderer.ts
+++ src/renderer/TextRenderer.ts
@@ -8,12 +8,12 @@ export class TextRenderer implements Renderer {
   render(scene: Scene) {
     console.clear();
     console.log(`=== FRAME START ===`);
-    console.log(`Cam Pos: ${JSON.stringify(scene.camera.position)}`);
+    console.log(`Cam Pos: ${JSON.stringify(scene.mainCamera.position)}`);
     console.log(`Entities: ${scene.entities.length}`);
     
     scene.entities.forEach(e => {
         // 简单的可视化：打印 ID 和 坐标
-        console.log(`[${e.id}] pos:(${e.position.x.toFixed(1)}, ${e.position.y.toFixed(1)})`);
+        console.log(`[${e.id}] pos:(${e.position[0].toFixed(1)}, ${e.position[1].toFixed(1)})`);
     });
     console.log(`=== FRAME END ===`);
   }
