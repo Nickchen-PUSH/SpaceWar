807fd8e3ecb597fdebd029aad8176d97a1ef34ef
Chen Haoquan <chenhq79@mail2.sysu.edu.cn>
2026-01-21 00:15:32 +0800

    [FEAT] tfighter fire

--- src/game/ships/t-fighter.ts
+++ src/game/ships/t-fighter.ts
@@ -2,20 +2,25 @@ import type { CameraView } from "@game/cameracontrollers";
 import { Ship } from "./Ship";
 import { vec3 } from "gl-matrix";
 import type { Game } from "@core/Game";
-import { ThrusterFlame } from "../effects/ThrusterFlame";
+import { Debug, LogChannel } from "@core/Debug";
+import { Bullet } from "@game/objects/Bullet";
 
 export class TFighter extends Ship {
     protected maxSpeed: number = 100;  // [m/s]
-    protected maxAcceleration: number = 20;   // [m/s²]
-    protected maxAngularSpeed: number = 10.0;  // [rad/s]
-    protected maxAngularAcceleration: vec3 = vec3.fromValues(10, 10, 10);  // [rad/s²] 每个轴的最大角加速度[pitch, yaw, roll]
+    protected maxAcceleration: number = 20;  // [m/s²]
+    protected maxAngularSpeed: number = 5.0;  // [rad/s]
+    protected maxAngularAcceleration: vec3 = vec3.fromValues(5, 5, 5);  // [rad/s²] 每个轴的最大角加速度[pitch, yaw, roll]
 
-    public maxHealth: number = 20;
-    public health: number = 20;
+    public maxHealth: number = 40;
+    public health: number = 40;
     public shield: number = 10;
 
     // 碰撞半径（球形）
     public hitRadius: number = 8.0;
+
+    // 子弹间隔
+    private fireCooldown = 1.0;
+    private fireTimer = 0;
 
     protected cameraView: CameraView = {
         cockpitOffset: vec3.fromValues(0, 0.85, -1),
@@ -44,5 +49,31 @@ export class TFighter extends Ship {
     protected onUpdate(_delta: number): void {
         void _delta;
         // 这里可以添加 TFighter 特有的更新逻辑
+        this.fireTimer += _delta;
+        
+        if (this.isFiring && this.fireTimer >= this.fireCooldown) {
+            this.fire();
+            this.fireTimer = 0;
+        }
+        this.isFiring = false;
     }
+
+    private fire(): void {
+            const muzzleOffset = vec3.fromValues(0, 0, 0);
+            const worldMuzzle = vec3.create();
+            vec3.transformQuat(worldMuzzle, muzzleOffset, this.rotation);
+            vec3.add(worldMuzzle, worldMuzzle, this.position);
+    
+            // 子弹方向应由飞船自身朝向决定（不受摄像机影响）
+            const finalFront = this.getFront();
+            Debug.log(LogChannel.GameLogic, `TFighter firing bullet from ${vec3.str(worldMuzzle)} towards ${vec3.str(finalFront)}`);
+    
+            const bullet = new  Bullet(
+                worldMuzzle,
+                finalFront,
+                this,
+                this.game
+            );
+            this.game.getScene().add(bullet);
+        }
 }
